21c8a7616de57dfc725a07626e0056db6ca3ef28
Mad-iq <debashritamandal852@gmail.com>
2025-12-15 12:27:23 +0530

    Added the user.model

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,19 @@
+export interface LoginRequest {
+  username: string;
+  password: string;
+}
+
+
+export interface AuthResponse {
+  token?: string;
+  id?: number;
+  role?: string;
+  error?: string;
+}
+
+
+export interface User {
+  id: number;
+  username: string;
+  role: string;
+}
